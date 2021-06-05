985788d178a3397a64d38d4b0d143faabf2d2cfe
Gary Berkinfand <gberkinfand@gmail.com>
2021-06-06 01:31:58 +0300

    Initialising Lottery

---
+++ Lottery/test/Lottery.test.js
@@ -0,0 +1,6 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+const {interface, bytecode} = require('../compile');
