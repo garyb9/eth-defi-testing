78816896dd6691ae9de5002d288b8841a18bacb5
Gary Berkinfand <gberkinfand@gmail.com>
2021-06-27 01:52:55 +0300

    deploying RPS+stringUtils

--- RPS/scripts/deploy.js
+++ RPS/scripts/deploy.js
@@ -1,5 +1,8 @@
 // npx hardhat run scripts/deploy.js --network <network-name>
 
+// const { ethers, config } = require("@nomiclabs/buidler");
+// const { readArtifact } = require("@nomiclabs/buidler/plugins");
+
 async function main() {
 
     const [deployer] = await ethers.getSigners();
@@ -10,13 +13,24 @@ async function main() {
     );
     
     console.log("Account balance:", (await deployer.getBalance()).toString());
-  
-    const Contract = await ethers.getContractFactory("RPS");
+    
+    // Deployment of libraries
+    const Library = await ethers.getContractFactory("StringUtils");
+    const library = await Library.deploy();
+    await library.deployed();   
+    console.log("Library address:", library.address);
+    
+    // Deployment of Contracts, linking with libraries
+    const Contract = await ethers.getContractFactory("RPS", {
+        libraries: {
+            StringUtils: library.address,
+        },
+    });
     const contract = await Contract.deploy();
-  
+    await contract.deployed();
     console.log("Contract address:", contract.address);
   }
-  
+
   main()
     .then(() => process.exit(0))
     .catch(error => {
