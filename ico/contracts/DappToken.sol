58e9ec6473853bde8d9a0918d52a8b792a371c36
Gary Berkinfand <gberkinfand@gmail.com>
2021-06-27 16:22:22 +0300

    initializing DappToken

---
+++ ico/contracts/DappToken.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract DappToken /*is ERC20*/ {
+
+    // Variables
+    uint public totalSupply;
+
+    // Events
+
+
+    // Constructor
+    constructor(){
+        totalSupply = 1000000; // 1 Mill supply
+    }
+
+    // Functions
+}
