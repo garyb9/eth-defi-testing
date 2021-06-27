9f4d3d9d808df9df4f663bd707e04dfab30b3e37
Gary Berkinfand <gberkinfand@gmail.com>
2021-06-27 13:11:11 +0300

    removing href's

--- dBank/src/components/App.js
+++ dBank/src/components/App.js
@@ -56,7 +56,7 @@ class App extends Component {
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
           <a
             className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
+            href="#"
             target="_blank"
             rel="noopener noreferrer"
           >
