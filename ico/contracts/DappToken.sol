39b22bcbb42543bc3d997af7f188fbfa40cc58e2
Gary Berkinfand <gberkinfand@gmail.com>
2021-06-28 17:55:11 +0300

    adding approval and transferFrom

--- ico/contracts/DappToken.sol
+++ ico/contracts/DappToken.sol
@@ -10,14 +10,21 @@ contract DappToken{ /* is ERC20 */
     string  public symbol    = "DAPP";
     string  public standard  = "Dapp Token v1.0";
     address public minter;
-    uint    public totalSupply;
-    mapping (address => uint) public balanceOf;
-     
+    uint256 public totalSupply;
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping(address => uint256)) public allowance;
+
     // Events
     event Transfer(
         address indexed _from, 
         address indexed _to, 
         uint256 value
+    );
+
+    event Approve(
+        address indexed _owner, 
+        address indexed _spender,
+        uint256 _value
     );
 
     // Constructor
@@ -28,13 +35,31 @@ contract DappToken{ /* is ERC20 */
     }
 
     // Functions
-    function transfer(address _to, uint _value) public returns(bool success){
+    function transfer(address _to, uint256 _value) public returns(bool success){
         require(_value <= totalSupply, "Error: transfer value is greater than totalSupply!");
         require(_value <= balanceOf[msg.sender], "Error: transfer value is greater than balanceOf sender!");
 
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
         emit Transfer(msg.sender, _to, _value);
+        return success = true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success){
+        allowance[msg.sender][_spender] = _value;
+        emit Approve(msg.sender, _spender, _value);
+        return success = true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns(bool success){
+        require(_value <= totalSupply, "Error: transfer value is greater than totalSupply!");
+        require(_value <= balanceOf[_from], "Error: transfer value is greater than balanceOf _from!");
+        require(_value <= allowance[_from][msg.sender], "Error: transfer value is greater than allowance of _from!");
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        allowance[_from][msg.sender] -= _value;
+        emit Transfer(_from, _to, _value);
         return success = true;
     }
 }
